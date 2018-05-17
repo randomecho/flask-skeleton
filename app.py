d6b3b05c03003687cdbc5cde050b6e3303620d35
Soon Van <cog@randomecho.com>
2018-05-17 14:08:24 -0400

    Catch 404 Not Found errors on missing pages

--- app.py
+++ app.py
@@ -51,5 +51,9 @@ def repos():
 def websites():
     return render_template("websites.html", site_links = site_links, slug = "websites")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("missing.html", site_links = site_links), 404
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
